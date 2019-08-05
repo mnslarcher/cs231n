c46a9847ff4beb4257aed466fd905191f0df2926
Mario Namtao Shianti Larcher <mnslarcher@gmail.com>
2019-08-05 15:57:16 +0200

    softmax done

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -14,7 +14,7 @@ def softmax_loss_naive(W, X, y, reg):
     - W: A numpy array of shape (D, C) containing weights.
     - X: A numpy array of shape (N, D) containing a minibatch of data.
     - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-      that X[i] has label c, where 0 <= c < C.
+    that X[i] has label c, where 0 <= c < C.
     - reg: (float) regularization strength
 
     Returns a tuple of:
@@ -33,7 +33,33 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    N = X.shape[0]
+    C = W.shape[1]
+
+    for i in range(N):
+        zi = []
+        xi = X[i]
+
+        for j in range(C):
+            zij = xi.dot(W[:, j])
+            zi.append(zij)
+  
+        exp_zi = np.exp(zi - np.max(zi))
+
+        pi = exp_zi / exp_zi.sum()
+        
+        p_yi = pi[y[i]]
+        loss += -np.log(p_yi)
+
+        grad = xi[:, np.newaxis] * pi
+        grad[:, y[i]] = (p_yi - 1) * xi
+        dW += grad
+    
+    loss /= N
+    dW /= N
+
+    loss += reg * np.sum(W * W)
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -57,8 +83,22 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    N = X.shape[0]
 
-    pass
+
+    Z = X.dot(W)
+    Z -= Z.max(axis=1, keepdims=True)
+    exp_Z = np.exp(Z)
+    P = exp_Z / exp_Z.sum(axis=1, keepdims=True)
+
+    loss = -np.log(P[range(N), y]).mean()
+
+    P[range(N), y] -= 1 
+
+    dW = X.T.dot(P) / N
+
+    loss += reg * np.sum(W * W)
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
